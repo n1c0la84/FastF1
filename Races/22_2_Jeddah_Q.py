82a963e111d5333ff129fbbcf853661bc2fff6b8
n1c0la84 <nicola084@gmail.com>
2022-03-31 17:18:44 +0200

    31/03

--- Races/22_2_Jeddah_Q.py
+++ Races/22_2_Jeddah_Q.py
@@ -13,9 +13,10 @@ session = fastf1.get_session(2022, 'Jeddah', 'Q')
 session.load()
 
 myplots = myfastf1plots.MyFastF1Plots()
-#myplots.speed_plot(plot_title='LEC vs PER', session=session, driver1='LEC', driver1_color='red', driver2='PER',
+# myplots.speed_plot(plot_title='LEC vs PER', session=session, driver1='LEC', driver1_color='red', driver2='PER',
 #                   driver2_color='blue')
-myplots.telemetry_plot(plot_title='LEC vs PER', session=session, driver1='LEC', driver1_color='red',
-                       driver2='PER', driver2_color='blue')
+myplots.telemetry_plot(session=session, driver1='LEC', driver1_color='red',
+                       driver2='PER', driver2_color='blue'
+                       )
 
 myplots.show_plots()
--- myfastf1plots.py
+++ myfastf1plots.py
@@ -2,52 +2,72 @@ from matplotlib import pyplot as plt
 
 
 class MyFastF1Plots:
-    def laptime_plot(self, plot_title, session, driver1, driver1_color, driver2=None, driver2_color=None):
+    def laptime_plot(self, session, driver1, driver1_color, driver2=None, driver2_color=None, plot_title=None):
         driver1_ = session.laps.pick_driver(driver1)
-
-        if driver2:
-            driver2_ = session.laps.pick_driver(driver2)
 
         fig, ax = plt.subplots()
         ax.plot(driver1_['LapNumber'], driver1_['LapTime'], driver1_color)
 
         if driver2:
+            driver2_ = session.laps.pick_driver(driver2)
             ax.plot(driver2_['LapNumber'], driver2_['LapTime'], driver2_color)
+
+        if not plot_title:
+            plot_title = driver1
+
+        if driver2 and plot_title == driver1:
+            plot_title = plot_title + " vs " + driver2
 
         ax.set_title(plot_title)
         ax.set_xlabel("Lap Number")
         ax.set_ylabel("Lap Time")
 
-    def speed_plot(self, plot_title, session, driver1, driver1_color, driver2=None, driver2_color=None):
+    def speed_plot(self, session, driver1, driver1_color, driver2=None, driver2_color=None, plot_title=None):
         driver1_fastest = session.laps.pick_driver(driver1).pick_fastest()
         driver1_car_data = driver1_fastest.get_car_data()
         driver1_time = driver1_car_data['Time']
         driver1_speed = driver1_car_data['Speed']
+
+        fig, ax = plt.subplots()
+        ax.plot(driver1_time, driver1_speed, driver1_color)
 
         if driver2:
             driver2_fastest = session.laps.pick_driver(driver2).pick_fastest()
             driver2_car_data = driver2_fastest.get_car_data()
             driver2_time = driver2_car_data['Time']
             driver2_speed = driver2_car_data['Speed']
-
-        fig, ax = plt.subplots()
-        ax.plot(driver1_time, driver1_speed, driver1_color)
-
-        if driver2:
             ax.plot(driver2_time, driver2_speed, driver2_color)
+
+        if not plot_title:
+            plot_title = driver1
+
+        if driver2 and plot_title == driver1:
+            plot_title = plot_title + " vs " + driver2
 
         ax.set_title(plot_title)
         ax.set_xlabel("Time")
         ax.set_ylabel("Speed [Km/h]")
         ax.legend()
 
-    def telemetry_plot(self, plot_title, session, driver1, driver1_color, driver2=None, driver2_color=None):
+    def telemetry_plot(self, session, driver1, driver1_color, driver2=None, driver2_color=None, plot_title=None):
         driver1_fastest_lap = session.laps.pick_driver(driver1).pick_fastest()
         driver1_car_data = driver1_fastest_lap.get_car_data()
         driver1_time = driver1_car_data['Time']
         driver1_speed = driver1_car_data['Speed']
         driver1_throttle = driver1_car_data['Throttle']
         driver1_brake = driver1_car_data['Brake'] * 100
+
+        if not driver2:
+            driver1_throttle_color = "green"
+            driver1_brake_color = "red"
+        else:
+            driver1_throttle_color = driver1_color
+            driver1_brake_color = driver1_color
+
+        fig, axs = plt.subplots(3)
+        axs[0].plot(driver1_time, driver1_speed, driver1_color)
+        axs[1].plot(driver1_time, driver1_throttle, driver1_throttle_color)
+        axs[2].plot(driver1_time, driver1_brake, driver1_brake_color)
 
         if driver2:
             driver2_fastest_lap = session.laps.pick_driver(driver2).pick_fastest()
@@ -56,21 +76,15 @@ class MyFastF1Plots:
             driver2_speed = driver2_car_data['Speed']
             driver2_throttle = driver2_car_data['Throttle']
             driver2_brake = driver2_car_data['Brake'] * 100
-            driver1_throttle_color = driver1_color
-            driver1_brake_color = driver1_color
-        else:
-            driver1_throttle_color = "green"
-            driver1_brake_color = "red"
-
-        fig, axs = plt.subplots(3)
-        axs[0].plot(driver1_time, driver1_speed, driver1_color)
-        axs[1].plot(driver1_time, driver1_throttle, driver1_throttle_color)
-        axs[2].plot(driver1_time, driver1_brake, driver1_brake_color)
-
-        if driver2:
             axs[0].plot(driver2_time, driver2_speed, driver2_color)
             axs[1].plot(driver2_time, driver2_throttle, driver2_color)
             axs[2].plot(driver2_time, driver2_brake, driver2_color)
+
+        if not plot_title:
+            plot_title = driver1
+
+        if driver2 and plot_title == driver1:
+            plot_title = plot_title + " vs " + driver2
 
         axs[0].set_title(plot_title)
         axs[0].set_ylabel("Speed [Km/h]")
